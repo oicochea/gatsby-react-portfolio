a11efe8899243dcba9cfb2c3b3523cd17e90f1f2
Oscar Icochea Calenzani <o.icochea09@gmail.com>
2020-12-05 00:12:53 -0500

    added underline effect on links

--- src/components/header.js
+++ src/components/header.js
@@ -5,10 +5,10 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header>
     <nav className="topNav">
-      <a target= "_blank" href="https://github.com/oicochea"> Github</a>
-      <a target= "_blank"href="https://www.linkedin.com/in/oscaricocheacalenzani/"> Linkedin</a>
-      <a target= "_blank"href="#">Projects</a>
-      <a target= "_blank"href="https://docs.google.com/document/d/e/2PACX-1vQ8V0YglEIJD2cpvuDU3zPeFE-O2UnALJFHtFKbFrIR4VFWuqr5Xww9Lf7MHDO60yiSBaWEzPzkAwWc/pub">Resume</a>
+      <a className="topLinks" target= "_blank" href="https://github.com/oicochea"> Github</a>
+      <a className="topLinks" target= "_blank"href="https://www.linkedin.com/in/oscaricocheacalenzani/"> Linkedin</a>
+      <a className="topLinks" target= "_blank"href="#">Projects</a>
+      <a className="topLinks"target= "_blank"href="https://docs.google.com/document/d/e/2PACX-1vQ8V0YglEIJD2cpvuDU3zPeFE-O2UnALJFHtFKbFrIR4VFWuqr5Xww9Lf7MHDO60yiSBaWEzPzkAwWc/pub">Resume</a>
     </nav>
     {/* <div
       style={{
