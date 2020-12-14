13d3bfb6306faab75fb635f0e136bb02a899b7e3
Oscar Icochea Calenzani <o.icochea09@gmail.com>
2020-12-14 02:56:07 -0500

    footer

--- src/components/contactme.js
+++ src/components/contactme.js
@@ -4,12 +4,11 @@ import React from "react"
 
 const Contact = () => {
     return (
-    <div className="contactMeContainer" id="contactMeContainer">
+        <footer className="contactMeContainer" id="contactMeContainer">
         <h3>Oscar Icochea Calenzani </h3>
         <h3>Email: O.Icochea09@gmail.com</h3>
         <h3>Phone : 631-297-4172</h3>
-    </div>
-
+    </footer>
     )
 }
 
